528a5cede18a843faddd1ebdfb642aaa89de206e
Petr Viktorin <encukou@gmail.com>
2019-01-24 18:27:22 +0100

    Wi-Fi improvements

--- wifi/ap.py
+++ wifi/ap.py
@@ -1,5 +1,6 @@
 import network
 ap = network.WLAN(network.AP_IF)
 ap.active(True)
-ap.config(essid='upy_ap', password='pass123', channel=7)
+ap.config(essid='upy_ap', password='pass1234',
+          channel=7, authmode=network.AUTH_WPA2_PSK)
 print(ap.ifconfig())
